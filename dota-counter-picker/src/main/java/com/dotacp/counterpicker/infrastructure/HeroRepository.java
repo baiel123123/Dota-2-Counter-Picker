e33c3d7045764b1aca4dd7f834c3fd1c4847f867
Baiel <nurdinovbaiel2005@gmail.com>
2026-03-09 15:39:07 +0600

    feat: new method for finding heroes by name

--- dota-counter-picker/src/main/java/com/dotacp/counterpicker/infrastructure/HeroRepository.java
+++ dota-counter-picker/src/main/java/com/dotacp/counterpicker/infrastructure/HeroRepository.java
@@ -4,7 +4,9 @@ import com.dotacp.counterpicker.domain.Hero;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface HeroRepository extends JpaRepository<Hero, Long> {
     List<Hero> findAllByPrimaryAttr(String attribute);
+    Optional<Hero> findByLocalizedNameIgnoreCase(String localizedName);
 }
